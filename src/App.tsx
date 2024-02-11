af0cf6f23b5676a8626b825b554e45c376d8b469
neequu <nist1701@gmail.com>
2024-02-11 07:14:03 +0300

    feat: setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+
 function App() {
-  return <div className='text-pink-600'>helllo</div>
+  return (
+    <>
+      <NavLink to='/' className='text-pink-400'>
+        hello
+      </NavLink>
+      <NavLink to='/about' className='text-pink-400'>
+        ab
+      </NavLink>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App
+function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+}
+
+export default WrappedApp
