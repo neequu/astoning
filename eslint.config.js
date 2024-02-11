689deb3a793292c0eb56479d07f882bf060b7e7b
neequu <nist1701@gmail.com>
2024-02-11 07:23:08 +0300

    chore: setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  react: true,
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/jsx-no-useless-fragment': 'error',
+    'no-unused-expressions': 'error',
+  },
+})
