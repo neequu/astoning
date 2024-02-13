a4530efa9181432a5053d71056be25af4a7decd5
neequu <nist1701@gmail.com>
2024-02-13 19:09:02 +0300

    chore: add prop-types

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,13 +1,14 @@
+import PropTypes from 'prop-types'
 import { Loader2Icon } from 'lucide-react'
 
-interface Props {
-  size?: number
-}
-
-export function Spinner({ size = 36 }: Props) {
+export function Spinner({ size = 36 }) {
   return (
     <div className="animate-spin w-min h-min">
       <Loader2Icon size={size} />
     </div>
   )
+}
+
+Spinner.propTypes = {
+  size: PropTypes.number,
 }
