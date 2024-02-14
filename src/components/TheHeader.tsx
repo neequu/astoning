13bc2b47264199b48f588441af8bcf87bee64705
neequu <nist1701@gmail.com>
2024-02-14 18:37:05 +0300

    refactor: move logic from navbar to parent

--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -1,10 +1,26 @@
-import { NavLink } from 'react-router-dom'
-import { Button } from './ui/button'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
 import { Logo } from '@/components/misc/Logo'
 import { TheNav } from '@/components/TheNav'
 import { ThemeToggle } from '@/components/ThemeToggle'
+import supabase from '@/services/supabase'
+import { setUser } from '@/redux/slices/authSlice'
+import { Button } from '@/components/ui/button'
+import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks'
 
 export function TheHeader() {
+  const user = useAppSelector(state => state.auth.user)
+
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+
+  async function handleSignOut() {
+    await supabase.auth.signOut()
+    dispatch(setUser(null))
+    navigate('/login')
+    toast.success('signed out!')
+  }
+
   return (
     <header>
       <div className="container py-3 mx-auto px-4 flex justify-between items-center">
@@ -13,7 +29,7 @@ export function TheHeader() {
             <Logo />
           </NavLink>
         </Button>
-        <TheNav>
+        <TheNav handleSignOut={handleSignOut} user={user}>
           <ThemeToggle />
         </TheNav>
       </div>
--- src/components/TheNav.tsx
+++ src/components/TheNav.tsx
@@ -1,27 +1,15 @@
-import { NavLink, useNavigate } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { Clock8Icon, HeartIcon, LogOutIcon } from 'lucide-react'
-import { toast } from 'sonner'
+import type { User } from '@supabase/supabase-js'
 import { Button } from '@/components/ui/button'
-import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks'
-import supabase from '@/services/supabase'
-import { setUser } from '@/redux/slices/authSlice'
 
 interface Props {
   children?: React.ReactNode
+  handleSignOut: () => void
+  user: User | null
 }
 
-export function TheNav({ children }: Props) {
-  const user = useAppSelector(state => state.auth.user)
-  const dispatch = useAppDispatch()
-  const navigate = useNavigate()
-
-  async function signOut() {
-    await supabase.auth.signOut()
-    dispatch(setUser(false))
-    navigate('/login')
-    toast.success('signed out!')
-  }
-
+export function TheNav({ children, handleSignOut, user }: Props) {
   return (
     <nav className="flex items-center gap-2">
       {children}
@@ -38,7 +26,7 @@ export function TheNav({ children }: Props) {
                 <Clock8Icon className="h-[1.2rem] w-[1.2rem] scale-100 transition-all " />
               </NavLink>
             </Button>
-            <Button onClick={signOut} variant="outline" size="icon">
+            <Button onClick={handleSignOut} variant="outline" size="icon">
               <LogOutIcon className="h-[1.2rem] w-[1.2rem] scale-100 transition-all " />
             </Button>
 
