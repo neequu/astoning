f02280ea321f4bb79711f993c132e6508d9e2867
neequu <nist1701@gmail.com>
2024-02-14 23:36:14 +0300

    style: change image size

--- src/components/media/MediaCard.tsx
+++ src/components/media/MediaCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export function MediaCard({ item, isAuth, handleLike }: Props) {
   return (
-    <div className="shadow-lg border rounded-md overflow-hidden p-4 flex flex-col">
+    <div className="shadow-lg border rounded-md overflow-hidden sm:p-4 p-2 flex flex-col">
       <div className="mb-4">
         <p className="font-semibold text-xl line-clamp-1" title={item.title}>{item.title}</p>
         <div className="flex gap-1 items-center text-muted-foreground ">
@@ -19,10 +19,8 @@ export function MediaCard({ item, isAuth, handleLike }: Props) {
           <p className="text-sm">{item.year}</p>
         </div>
       </div>
-      <Link to={`/anime/${item.mal_id}`}>
-        <div className="pb-2 h-[20rem]">
-          <img src={item.images.webp.image_url} className="max-h-full max-w-full mx-auto rounded" alt={item.title} />
-        </div>
+      <Link to={`/anime/${item.mal_id}`} className="max-h-72">
+        <img src={item.images.webp.image_url} className="h-72 min-w-30 w-full max-w-56 mx-auto rounded" alt={item.title} />
       </Link>
       <div className="flex justify-end flex-1 place-items-end mt-4">
         <Button size="icon" variant="ghost" onClick={() => handleLike(isAuth, item.mal_id)}>
