2311fed7a47d7dd3271928fa0f6b9fc20fcaf57f
neequu <nist1701@gmail.com>
2024-02-14 18:25:38 +0300

    feat: add user init hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ErrorBoundary } from 'react-error-boundary'
 import { Toaster } from 'sonner'
-import { useEffect } from 'react'
-import supabase from './services/supabase'
-import { useAppDispatch } from './hooks/reduxHooks'
-import { setUser } from './redux/slices/authSlice'
+import { useInitializeUser } from '@/hooks/useInitializeUser'
 import { ThemeProvider } from '@/providers/theme-provider'
 import { LayoutWrapper } from '@/components/wrappers/LayoutWrapper'
 import { ErrorLayout } from '@/components/ErrorLayout'
 import Router from '@/router'
 
 function App() {
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-    async function checkAuth() {
-      const { data: { user } } = await supabase.auth.getUser()
-      dispatch(setUser(!!user))
-    }
-
-    checkAuth()
-  }, [])
+  useInitializeUser()
 
   return (
     <LayoutWrapper>
