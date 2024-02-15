b96d0aa15ff3e744ee049055eff0d9129c0b08c0
neequu <nist1701@gmail.com>
2024-02-15 16:50:12 +0300

    chore: break check like function if no user

--- src/hooks/use-set-like.ts
+++ src/hooks/use-set-like.ts
@@ -1,28 +1,35 @@
 import { useEffect, useState } from 'react'
 import { getFavorite } from '@/services/db'
 
-export function useCheckFavorite(itemId: number) {
+export function useCheckFavorite(itemId: number, isAuth: boolean) {
   const [isActive, setIsActive] = useState(false)
   const [isLoadingLike, setIsLoadingLike] = useState(false)
 
   useEffect(() => {
-    const checkLike = async () => {
+    const checkInitialLike = async () => {
+      // skip check if no user
+      if (!isAuth)
+        return null
+
       setIsLoadingLike(true)
       try {
         const likedItem = await getFavorite(itemId)
 
         if (!likedItem || !likedItem.length)
-          return
+          return null
 
         setIsActive(!!likedItem)
       }
-      catch (e) {}
+      catch (e) {
+        console.error(e)
+        return null
+      }
       finally {
         setIsLoadingLike(false)
       }
     }
 
-    checkLike()
+    checkInitialLike()
   }, [])
 
   return { isActive, setIsActive, isLoadingLike }
