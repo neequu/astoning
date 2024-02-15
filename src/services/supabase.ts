ff8c6d8ae2808ecd75ba49cfe6e34c92e5ac58c4
neequu <nist1701@gmail.com>
2024-02-15 03:24:58 +0300

    chore: add types to db

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
+import type { Database } from '@/types/db'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
 
 export default supabase
