61f49362baefb4b2887ce03087cba5749b1637c7
neequu <nist1701@gmail.com>
2024-02-16 20:39:24 +0300

    chore: remove ts comments

--- src/redux/apis/db-api.ts
+++ src/redux/apis/db-api.ts
@@ -1,23 +1,20 @@
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { User } from '@supabase/supabase-js'
 import { likeService } from '@/services/like'
-import type { Tables } from '@/types/db'
 
 export const dbApi = createApi({
   reducerPath: 'db',
   baseQuery: fakeBaseQuery(),
   tagTypes: ['LikeById', 'Like'],
   endpoints: builder => ({
-    getFavorites: builder.query<Tables<'favorites'>[], User['id'] | undefined>({
-      // @ts-expect-error database types currently not working todo: fix
+    getFavorites: builder.query<{ item_id: number }[] | null, User['id'] | undefined>({
       queryFn: async (userId) => {
         const data = await likeService.getFavorites(userId)
         return { data }
       },
       providesTags: ['Like'],
     }),
-    getFavoritesById: builder.query<Tables<'favorites'>[], { itemId: number, userId: User['id'] | undefined }>({
-      // @ts-expect-error database types currently not working todo: fix
+    getFavoritesById: builder.query<{ item_id: number }[] | null, { itemId: number, userId: User['id'] | undefined }>({
       queryFn: async ({ itemId, userId }) => {
         const data = await likeService.getFavoriteById(itemId, userId)
         return { data }
