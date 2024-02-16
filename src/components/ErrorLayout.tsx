8aa09d5c7167453434b79c7a9860a0bcc9040b9f
neequu <nist1701@gmail.com>
2024-02-16 12:34:12 +0300

    style: consistent heading

--- src/components/ErrorLayout.tsx
+++ src/components/ErrorLayout.tsx
@@ -7,7 +7,7 @@ export function ErrorLayout({ resetErrorBoundary }: FallbackProps) {
   return (
     <PageWrapper className="place-content-center text-center items-center">
       <div>
-        <h1 className="font-bold text-xl">Oops... There was an error!</h1>
+        <h1 className="text-2xl sm:text-5xl font-bold mb-4">Oops... There was an error!</h1>
         <Button asChild variant="link" onClick={resetErrorBoundary}>
           <Link to="/" className="text-xl">
             Go Home
