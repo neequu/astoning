c9b1d65a6f2aa07a9736ab4c7ee9c36cfcac84d0
neequu <nist1701@gmail.com>
2024-02-17 23:56:50 +0300

    chore: remove user fetch from

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ErrorBoundary } from 'react-error-boundary'
 import { Toaster } from 'sonner'
-import { LoadingSkeleton } from './components/loadingState/LoadingSkeleton'
-import { useInitializeUser } from '@/hooks/use-initialize-user'
 import { ThemeProvider } from '@/providers/theme-provider'
 import { LayoutWrapper } from '@/components/wrappers/LayoutWrapper'
 import { ErrorLayout } from '@/components/ErrorLayout'
 import Router from '@/router'
-import { authService } from '@/services/auth'
 
 function App() {
-  const { isLoading } = useInitializeUser(authService.getUser)
-
   return (
-    // review: is this ok?
-    // не тернарный чтобы не ругался еслинт
-    <>
-      {isLoading && <LoadingSkeleton /> }
-      {!isLoading && <LayoutWrapper><Router /></LayoutWrapper>}
-    </>
+    <LayoutWrapper>
+      <Router />
+    </LayoutWrapper>
   )
 }
 
--- src/components/wrappers/LayoutWrapper.tsx
+++ src/components/wrappers/LayoutWrapper.tsx
@@ -1,16 +1,28 @@
+import { LoadingSkeleton } from '@/components/loadingState/LoadingSkeleton'
 import { TheHeader } from '@/components/TheHeader'
+import { useInitializeUser } from '@/hooks/use-initialize-user'
+import { authService } from '@/services/auth'
 
 interface Props {
   children: React.ReactNode
 }
 
 export function LayoutWrapper({ children }: Props) {
+  const { isLoading } = useInitializeUser(authService.getUser)
+
   return (
     <>
-      <TheHeader />
-      <main className="container mx-auto px-4 flex-1 flex flex-col">
-        {children}
-      </main>
+      {isLoading && <LoadingSkeleton /> }
+      {!isLoading
+      && (
+        <>
+          <TheHeader />
+          <main className="container mx-auto px-4 flex-1 flex flex-col">
+            {children}
+          </main>
+        </>
+      )}
+
     </>
   )
 }
