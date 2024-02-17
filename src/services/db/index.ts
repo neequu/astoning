2c4f02359bbc8aee902f9a9b5360f4548729a6b8
neequu <nist1701@gmail.com>
2024-02-17 03:25:26 +0300

    chore: update db types

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -1,12 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
-
-// import type { Database } from '@/types/db'
+import type { Database } from '@/types/db'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-// todo: setup db types
 
-// const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
 
 export default supabase
