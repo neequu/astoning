60db3d1bbd02627545a0e4cd06e32e625b6e91c9
neequu <nist1701@gmail.com>
2024-02-20 02:59:13 +0300

    chore: remove comments

--- src/services/db/localstorage/methods/index.ts
+++ src/services/db/localstorage/methods/index.ts
@@ -1,7 +1,3 @@
-// export * from './favorites'
-// export * from './history'
-// export * from './auth'
-
-export { getUser, loginWithCredentials, loginWithOAuth, register, signOut } from './auth'
-export { addFavorite, getFavoriteById, getFavorites, removeFavorite } from './favorites'
-export { addHistory, deleteAllHistory, deleteHistoryById, getHistory } from './history'
+export * from './favorites'
+export * from './history'
+export * from './auth'
