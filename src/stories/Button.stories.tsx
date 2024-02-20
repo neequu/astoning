8b1cc51d27c26b580a39c23fabb431eb3901b1b6
neequu <nist1701@gmail.com>
2024-02-20 20:49:48 +0300

    feat: add stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,18 +1,25 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { boolean, text, withKnobs } from '@storybook/addon-knobs'
 import { Button as ButtonUI } from '@/components/ui/button'
 
 const meta = {
   title: 'ui/button',
   component: ButtonUI,
+  decorators: [withKnobs],
   tags: ['autodocs'],
   parameters: {
     layout: 'centered',
   },
   args: {
-    children: 'Hello',
-    className: 'bg-pink-400 p-20',
+    children: text('label', 'Hello'),
+    className: text('class', ''),
+    disabled: boolean('disabled', false),
   },
 } satisfies Meta<typeof ButtonUI>
+
+export function KnobsButton() {
+  return <ButtonUI disabled={boolean('disabled', false)}>{text('label', 'b-b-button')}</ButtonUI>
+}
 
 export default meta
 
--- src/stories/Spinner.stories.tsx
+++ src/stories/Spinner.stories.tsx
@@ -1,16 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { number, text, withKnobs } from '@storybook/addon-knobs'
 import { Spinner as SpinnerUI } from '@/components/ui/spinner'
 
 const meta = {
   title: 'ui/spinner',
   component: SpinnerUI,
+  decorators: [withKnobs],
   tags: ['autodocs'],
   parameters: {
     layout: 'centered',
   },
   args: {
-    className: 'text-pink-400',
-    size: 36,
+    className: text('class', ''),
+    size: number('size', 36),
   },
 } satisfies Meta<typeof SpinnerUI>
 
@@ -19,3 +21,12 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Spinner: Story = {}
+
+export function KnobsSpinner() {
+  return (
+    <SpinnerUI
+      className={text('class', '')}
+      size={number('size', 36)}
+    />
+  )
+}
