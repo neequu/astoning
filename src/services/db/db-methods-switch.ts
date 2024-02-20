12769c4bea9d7ebc2c54af2b20091e15428c0afe
neequu <nist1701@gmail.com>
2024-02-20 03:43:31 +0300

    chore: rename env var

--- src/services/db/db-methods-switch.ts
+++ src/services/db/db-methods-switch.ts
@@ -2,6 +2,6 @@
 import * as supabaseMethods from './supabase/methods'
 import * as LSMethods from './localstorage/methods'
 
-const isDbSupabase = import.meta.env.VITE_DB === 'SUPABASE'
+const isDbSupabase = import.meta.env.REMOTE_STORE === 'SUPABASE'
 
 export default isDbSupabase ? supabaseMethods : LSMethods
