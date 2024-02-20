019632c68797a9624b3a5696d18994c93d6a9daa
neequu <nist1701@gmail.com>
2024-02-20 17:33:44 +0300

    chore: add loading state to suggestions

--- src/components/search/SearchSuggestions.tsx
+++ src/components/search/SearchSuggestions.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react'
 import { lazily } from 'react-lazily'
+import { CardSkeleton } from '../loading-state/CardSkeleton'
 import { MAX_SUGGESTIONS } from '@/lib/constants'
 import { useGetAnimeSearchQuery } from '@/redux/api/anime-api'
 import { cn } from '@/lib/utils'
@@ -14,7 +15,7 @@ interface Props {
 }
 
 export function SearchSuggestions({ debouncedQuery, isInputFocused }: Props) {
-  const { data: searchData, isSuccess, isError, isFetching } = useGetAnimeSearchQuery({ q: debouncedQuery, limit: MAX_SUGGESTIONS }, {
+  const { data: searchData, isSuccess, isError, isFetching, isLoading } = useGetAnimeSearchQuery({ q: debouncedQuery, limit: MAX_SUGGESTIONS }, {
     skip: !debouncedQuery,
   })
   // don't render anything initially
@@ -25,12 +26,16 @@ export function SearchSuggestions({ debouncedQuery, isInputFocused }: Props) {
 
   return (
     <div className={cn('flex flex-col bg-menu text-menu-foreground rounded-md shadow-lg border mt-3 overflow-hidden absolute top-full -left-20 -right-20 z-50 opacity-0 transition-all duration-500', isInputFocused && 'opacity-100 left-0 right-0')}>
-      <Suspense>
-        <AnimationWrapper>
-          {searchData.data.map(item => (
-            <SearchSuggestionCard key={item.malId} item={item} />
-          ))}
-        </AnimationWrapper>
+      <Suspense fallback={<CardSkeleton amount={5} className="h-[81px]" />}>
+        {isLoading
+          ? <CardSkeleton amount={5} className="h-[80px]" />
+          : (
+            <AnimationWrapper>
+              {searchData.data.map(item => (
+                <SearchSuggestionCard key={item.malId} item={item} />
+              ))}
+            </AnimationWrapper>
+            )}
         {isError && <Message message="There was an error with your search!" className="flex-1 text-xl items-center text-destructive py-7" />}
         {noResults && !isFetching && <Message message={`No results for ${debouncedQuery} found!`} className="flex-1 items-center py-7 text-md" />}
       </Suspense>
