346f3b1b114cd220a307963afa759d8afbf7528c
neequu <nist1701@gmail.com>
2024-02-23 17:03:17 +0300

    chore: lint fix

--- src/providers/feature-flag-provider.tsx
+++ src/providers/feature-flag-provider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext } from 'react'
 import { useGetFeatureFlagQuery } from '@/features/api/telegram'
 import type { FeatureState } from '@/types/features'
 
