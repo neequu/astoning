e85fecba62c169b2030d35dba060b39838efedd5
neequu <nist1701@gmail.com>
2024-02-23 12:53:06 +0300

    chore: add types

--- src/services/console/methods/history.ts
+++ src/services/console/methods/history.ts
@@ -2,22 +2,22 @@
 import { getUser } from './auth'
 import databaseMethods from '@/services/db/db-methods-switch'
 
-export async function getHistory() {
+export async function getHistory(): Promise<void> {
   const user = await getUser()
   databaseMethods.getHistory(user?.id)
     .then(res => console.log(res))
 }
-export async function addHistory(params: string[]) {
+export async function addHistory(params: string[]): Promise<void> {
   const user = await getUser()
   const query = params[0]
   databaseMethods.addHistory(query, user?.id)
 }
-export async function deleteHistoryById(params: string[]) {
+export async function deleteHistoryById(params: string[]): Promise<void> {
   const user = await getUser()
   const id = +params[0]
   databaseMethods.deleteHistoryById(id, user?.id)
 }
-export async function deleteAllHistory() {
+export async function deleteAllHistory(): Promise<void> {
   const user = await getUser()
   databaseMethods.deleteAllHistory(user?.id)
 }
