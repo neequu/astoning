53ccc6538e8419a32313621f47583ff1daf18ce1
neequu <nist1701@gmail.com>
2024-02-24 23:27:25 +0300

    fix: fix fallback url

--- src/features/telegram.ts
+++ src/features/telegram.ts
@@ -1,6 +1,6 @@
 import type { FeatureState } from '@/types/features'
 
-const serverUrl = import.meta.env.VITE_SERVER_URL + import.meta.env.VITE_PORT || 3002
+const serverUrl = import.meta.env.VITE_SERVER_URL + (import.meta.env.VITE_PORT || 3002)
 const featUrl = import.meta.env.VITE_TELEGRAM_FEAT_URL
 
 export async function getTelegramFeatureFlag(): Promise<FeatureState> {
