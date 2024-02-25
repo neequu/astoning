1fc3c4001ac608bd4bf7819568ba2f4b49b2973a
neequu <nist1701@gmail.com>
2024-02-25 17:56:48 +0300

    chore: change conosle commands

--- src/services/console/commands.ts
+++ src/services/console/commands.ts
@@ -1,18 +1,18 @@
 export const commands = {
-  'anime-get-all': '/anime',
-  'anime-get-one': '/anime/',
-  'anime-search': '/search',
+  'anime-get-all': 'anime/all',
+  'anime-get-one': 'anime',
+  'anime-search': 'search',
 
-  'like-add': '/like',
-  'like-remove': '/like/rm',
-  'likes-get-all': '/likes',
+  'like-add': 'like',
+  'like-remove': 'like/rm',
+  'likes-get-all': 'likes',
 
-  'login': '/login',
-  'register': '/register',
-  'signout': '/signout',
+  'login': 'login',
+  'register': 'register',
+  'signout': 'signout',
 
-  'history-get-all': '/history',
-  'history-remove-one': '/history/rm',
-  'history-remove-all': '/history/clear',
+  'history-get-all': 'history',
+  'history-remove-one': 'history/rm',
+  'history-remove-all': 'history/clear',
 }
 export const availableCommands = Object.entries(commands)
